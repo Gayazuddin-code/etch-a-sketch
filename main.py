34f294225b434a9f4732a46eeedb4576671c54fe
lexi <lexi@leon>
2021-05-24 13:07:59 +0530

    complete basic functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 
 tim = Turtle()
+tim.pensize(5)
 screen = Screen()
 
 
@@ -8,7 +9,27 @@ def mov_forward():
     tim.forward(10)
 
 
+def mov_backward():
+    tim.backward(10)
+
+
+def counter_clock():
+    tim.left(10)
+
+
+def clock():
+    tim.right(10)
+
+
+def clear_screen():
+    screen.reset()
+
+
 screen.listen()
 screen.onkey(key="w", fun=mov_forward)
+screen.onkey(key="s", fun=mov_backward)
+screen.onkey(key="a", fun=counter_clock)
+screen.onkey(key="d", fun=clock)
+screen.onkey(key="c", fun=clear_screen)
 
 screen.exitonclick()
